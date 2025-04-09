ee01a8cbe296ccebe127ef62fa5e97ecd7a25493
Ramgopal-A <ramgopal.a1611@gmail.com>
2025-04-09 10:56:17 +0530

    hosted backend and the port has been changed in the js file

--- hospitalManagment/hospitalManagment/Program.cs
+++ hospitalManagment/hospitalManagment/Program.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
+
 // Load appsettings.json
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 builder.Configuration.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
@@ -42,6 +45,12 @@ builder.Services.AddControllers();
 
 
 var app = builder.Build();
+
+// Use PORT from environment (important for Render or any cloud)
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+Console.WriteLine($"Running on port: {port}");
+app.Urls.Add($"http://*:{port}");
 
 app.UseCors("AllowAllOrigins");
 app.UseRouting();
